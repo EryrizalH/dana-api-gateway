6abedb9c6a2ee062c3d6a5ba8a33509c1b5ba951
eryrizalH <eryrizal23@gmail.com>
2026-09-23 23:05:15 +0700

    fix: enable trust proxy for https protocol behind reverse proxy

--- server.js
+++ server.js
@@ -141,6 +141,7 @@ const authWeb = (req, res, next) => {
 };
 
 const app = express();
+app.enable('trust proxy');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
